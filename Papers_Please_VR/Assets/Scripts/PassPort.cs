82c93be76593f36574ee9ced35000f01eb7ab2d4
Hans (cgt103529) <cgt103529@stud.fh-wedel.de>
2023-04-01 18:04:26 +0200

    Passport and Computer

--- Papers_Please_VR/Assets/Scripts/PassPort.cs
+++ Papers_Please_VR/Assets/Scripts/PassPort.cs
@@ -1,12 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Numerics;
 using TMPro;
 using UnityEditor;
 using UnityEngine;
 using System.IO;
 
-
+#region Classes
+[System.Serializable]
 public class Name
 {
     public string name;
@@ -16,6 +16,8 @@ public class Names
 {
     public Name[] nameList;
 }
+
+[System.Serializable]
 public class LastName
 {
     public string name;
@@ -26,52 +28,103 @@ public class LastNames
     public LastName[] lastNameList;
 }
 
+[System.Serializable]
+public class Country
+{
+    public string name;
+}
+
+public class Countrys
+{
+    public Country[] countryList;
+}
+
+[System.Serializable]
+public class PassTyp
+{
+    public string name;
+}
+
+public class PassTypes
+{
+    public PassTyp[] passTypList;
+}
+#endregion
+
 public class PassPort : MonoBehaviour
 {
-    [SerializeField] TextMeshPro m_TextMeshPro;
-    [SerializeField] GameObject m_Picture;
+    #region PassInfo
+    string country;
     string passName;
+    string passLastName;
     int passAge;
+    Vector3Int expirationDate;
+    Vector3Int dateOfcreation;
+    Vector3Int dateOfBirth;
+    string passType;
+    #endregion
+
+    [SerializeField] TextMeshPro m_TextMeshPro;
+    [SerializeField] TextMeshPro backSideText;
+    [SerializeField] GameObject m_Picture;
+    
     Object [] m_Faces;
-    string textDataName = "";
+    string textDataName = "NameList2.json";//"NameList.json";
+    string datapath;
+    string passInfo;
 
     void Start()
     {
         GameEvents.current.onTriggerInfo += ReaderHit;
-        m_Faces = Resources.LoadAll("Faces", typeof(Renderer));
-        passName = "Guenther";
-        passAge = 32;
-        m_TextMeshPro.text = "Name " + passName + "<br><br>";
-        m_TextMeshPro.text += "Alter " + passAge;
-        Debug.Log(m_Faces.Length);
+        //m_Faces = Resources.LoadAll("Faces", typeof(Renderer));
+
+        //passAge = Random.Range(5, 92);
+        expirationDate = new Vector3Int(Random.Range(1, 30), Random.Range(1, 12), Random.Range(2015, 2035));
+        dateOfcreation = new Vector3Int(Random.Range(1, 30), Random.Range(1, 12), Random.Range(2015, 2035));
+        dateOfBirth = new Vector3Int(Random.Range(1, 30), Random.Range(1, 12), Random.Range(1920, 2035));
+
+        //Debug.Log(m_Faces.Length);
         //m_Picture.GetComponent<Renderer>().material = (Material) m_Faces[0]; 
+        datapath = Application.dataPath + "/Resources/" + textDataName;
 
-        
-    }
+        Names names;
+        LastNames lastNames;
+        Countrys countrys;
+        PassTypes passTypes;
 
-    void loadJson()
-    {
-        string datapath = Application.dataPath + "/Resources/" + textDataName;
-        Names peopleInfo;
         if (File.Exists(datapath))
         {
             string fileContents = File.ReadAllText(datapath);
-            peopleInfo = JsonUtility.FromJson<Names>(fileContents);
+            names = JsonUtility.FromJson<Names>(fileContents);
+            lastNames = JsonUtility.FromJson<LastNames>(fileContents);
+            countrys = JsonUtility.FromJson<Countrys>(fileContents);
+            passTypes = JsonUtility.FromJson<PassTypes>(fileContents);
 
-            foreach (Name p in peopleInfo.nameList)
-            {
-                
-            }
+            int rand = Random.Range(0, names.nameList.Length - 1);
+            passName = names.nameList[rand].name;
+            rand = Random.Range(0, lastNames.lastNameList.Length - 1);
+            passLastName = lastNames.lastNameList[rand].name;
+            rand = Random.Range(0, countrys.countryList.Length - 1);
+            country = countrys.countryList[rand].name;
+            rand = Random.Range(0, passTypes.passTypList.Length - 1);
+            passType = passTypes.passTypList[rand].name;
         }
         else
         {
-            //text.text = "Cannot Read PlanetList";
+            Debug.Log("Cannot Read PlanetList");
         }
+
+        m_TextMeshPro.text = "Type " + passType + "<br><br>";
+        m_TextMeshPro.text += "Name " + passLastName + "     Vorname " + passName + "<br><br>";
+        m_TextMeshPro.text += "Geburtsdatum " + dateOfBirth + "<br><br>";
+        m_TextMeshPro.text += "Ablaufdatum "+ expirationDate + "<br><br>" + "Erstelldatum " + dateOfcreation;
+        passInfo = m_TextMeshPro.text;
+        backSideText.text = country;
     }
 
     void ReaderHit()
     {
-        GameEvents.current.Info(passName);
+        GameEvents.current.Info(passInfo);
     }
 
     // Update is called once per frame
